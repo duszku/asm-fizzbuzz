20241052b24bfd3959fd413a5ff609d799675414
brzydkirumun <brzydkirumun@users.noreply.github.com>
2022-07-19 00:26:10 +0200

    string operations

--- main.s
+++ main.s
@@ -1,19 +1,17 @@
 section .data
-    welcome_msg db "Welcome to fizzbuzz!",10,"How far do you wish to go: "
+    welcome_msg db "Welcome to fizzbuzz!",0x0a,"How far do you wish to go: ",0x00
 
 section .bss
-    limit resb 16
+    limit resb 0xf0
 
 section .text
     global _start
 
     _start:
+        mov rdx,welcome_msg
+        call _putstr
 
-    _putstr:
-        pusha
-        mov rax,0x01
-        mov rdi,0x01
-        mov rdx,0xf0
-        syscall
-        popa
-        ret
+        mov rax,0x3b
+        mov rdi,0x00
+
+    %include strings.s
