644c4d4b294b7c032d1bf9f94d54d40cd4b03289
brzydkirumun <brzydkirumun@users.noreply.github.com>
2022-07-19 17:10:37 +0200

    Added gitignore

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -1,6 +1,8 @@
 section .data
     msg_1:  db "Welcome to Assembly fizzbuzz!",0x0a,0x00
     msg_2:  db "Enter a number: ",0x00
+    msg_3:  db "It was a number!",0x00
+    msg_4:  db "That is not a number!",0x00
 
 section .bss
     limit:  resb 0xf1
@@ -20,6 +22,22 @@ section .text
         mov     rsi, limit
         mov     rdx, 0xf1
         call    getstr
+
+        call    putstr
+
+        call    isnumber
+    if:
+        cmp     rax, 0x01
+        jne     else
+
+        mov     rsi, msg_3
+        call    putstr
+        jmp     fi
+    else:
+        mov     rsi, msg_4
+        call    putstr
+        jmp     fi
+    fi:
 
         mov     rax, 0x3c
         mov     rdi, 0x00
